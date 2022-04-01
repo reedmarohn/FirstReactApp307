d59bcaa6992f8db3595bcb09b44810a4b78a4e4d
reedmarohn <reedmarohn@yahoo.com>
2022-04-01 13:30:00 -0700

    Updated App with Remove button and Column Header

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import Table from './Table'
-
-const characters = [
-    {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-];
+import React, {useState} from 'react';
+import Table from './Table';
 
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: 'Charlie',
+      job: 'Janitor',
+    },
+    {
+      name: 'Mac',
+      job: 'Bouncer',
+    },
+    {
+      name: 'Dee',
+      job: 'Aspring actress',
+    },
+    {
+      name: 'Dennis',
+      job: 'Bartender',
+    },
+  ]);
+  
+function removeOneCharacter (index) {
+  const updated = characters.filter((character, i) => {
+    return i !== index 
+    //this return statement returns an array of all the characters besides the index given (so that its removed)
+  });
+  setCharacters(updated);
+}
     return (
         <div className="container">
-            <Table characterData={characters} />
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
         </div>
     );
 }
+
 
 export default MyApp;
